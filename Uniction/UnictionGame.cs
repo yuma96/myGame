fc857a70a7d4f4b2b6fbf85b5317d9e669a87c1c
yuma96 <35644016+yuma96@users.noreply.github.com>
2019-10-27 17:40:08 +0900

    ゲーム説明修正
    ゲーム説明が文字化けしてしまうため

---
+++ Uniction/UnictionGame.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnictionGame : MonoBehaviour {
+
+    /*
+     [ゲームユニクション]
+     ・操作説明
+     WASD : 移動
+     J,K,L: 攻撃
+     I,O  : カメラ回転
+
+     ・Unityのバージョン
+      2017.3.1f1(64-bit)
+
+     *パソコンスペック次第でラグが起きる可能性があります。
+     そうなった場合申し訳ございません。
+    */
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
